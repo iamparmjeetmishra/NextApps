bcd924aea374c5a153899446c6bb4f382cc8546d
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-17 00:14:55 +0530

    Featured Section is added + Background-gradient Used

--- 11nextmusicacademy/src/app/page.tsx
+++ 11nextmusicacademy/src/app/page.tsx
@@ -1,9 +1,10 @@
-import { HeroSection } from "@/components";
+import { HeroSection, FeaturedSection } from "@/components";
 
 export default function Home() {
   return (
     <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedSection />
     </main>
   );
 }
--- 11nextmusicacademy/src/components/index.js
+++ 11nextmusicacademy/src/components/index.js
@@ -1,12 +1,19 @@
 import Navbar from './Navbar'
 import HeroSection from './HeroSection'
+import FeaturedSection from './FeaturedSection'
+
 import { Spotlight } from './ui/Spotlight'
 import { MovingBorder, Button } from './ui/moving-border'
+import { BackgroundGradient } from './ui/background-gradient'
+
 
 export  {
     Navbar,
     HeroSection,
+    FeaturedSection,
+
     Spotlight,
     MovingBorder,
-    Button
+    Button,
+    BackgroundGradient,
 }
