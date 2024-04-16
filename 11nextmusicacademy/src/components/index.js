dd2178da38c0eefb520537b228260056ed5adc0f
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-16 23:40:22 +0530

    Hero section added + Spotlight Effect + Moving Border

---
+++ 11nextmusicacademy/src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+import HeroSection from './HeroSection'
+import { Spotlight } from './ui/Spotlight'
+import { MovingBorder, Button } from './ui/moving-border'
+
+export  {
+    Navbar,
+    HeroSection,
+    Spotlight,
+    MovingBorder,
+    Button
+}
