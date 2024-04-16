e2522ba468e8607422934863634d5ebbfba9738d
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-16 23:38:26 +0530

    hero Section is finished including Spotlight effect and moving border

--- 11nextmusicacademy/src/app/page.tsx
+++ 11nextmusicacademy/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { HeroSection } from "@/components";
 
 export default function Home() {
   return (
     <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h2>Next</h2>
+      <HeroSection />
     </main>
   );
 }
