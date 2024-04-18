22eb6f6dee040c55e4c1167dc10af47ad343946e
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-19 00:47:57 +0530

    Update the next config for Images

--- 11nextmusicacademy/next.config.mjs
+++ 11nextmusicacademy/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'res.cloudinary.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+            }
+        ]            
     }
 };
 
