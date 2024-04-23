a368591dabcdbf66736060a09d71b0ca06e283ab
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-23 16:42:43 +0530

    FE->Sign In Page Schema finish + Toaster

--- 12nexttruefeedback/src/app/(auth)/sign-in/page.tsx
+++ 12nexttruefeedback/src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,96 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useEffect, useState } from 'react'
+import { useDebounceValue} from 'usehooks-ts'
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { signUpSchema } from '@/schemas/signUpSchema'
+import axios, {AxiosError} from 'axios'
+import { ApiResponse } from '@/types/ApiResponse'
+import { useToast } from '@/components/ui/use-toast'
+import { Button } from '@/components/ui/button'
+
+
+export default function Page() {
+  
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const debouncedUsername = useDebounceValue(username, 300)
+  const router = useRouter()
+  const { toast } = useToast()
+
+  // zod implementation 
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    }
+  })
+
+  useEffect(() => { 
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
+          setUsernameMessage(response.data.message)
+
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(axiosError.response?.data.message ?? 'Error checking username')
+        } finally {
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUnique()
+
+  }, [debouncedUsername])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      console.log(data)
+      toast({
+        title: 'Success',
+        description: response.data.message
+      })
+      router.replace(`/verify/${username}`)
+      setIsSubmitting(false)
+    } catch (error) {
+      console.error('Error in signup of user', error)
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: 'Signup failed',
+        description: errorMessage,
+        variant: 'destructive'
+      })
+      setIsSubmitting(false)
+    }
   }
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-teal-600 px-4 py-2 hover:bg-teal-800" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className='flex justify-center items-center min-h-screen bg-gray-700'>
+      <div className='w-full max-w-md p-8 space-y-8 bg-white text-black rounded-lg shadow-md'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'>Join Anonymous Message</h1>
+          <p className='mb-4'>Sign up to start your anonymous adventure</p>
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+    </div>
   )
 }
