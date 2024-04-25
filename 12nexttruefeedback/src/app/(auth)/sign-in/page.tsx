b655ee46046cece51a90a0ee4999f4df5c328a67
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-25 11:37:37 +0530

    Sign In Page Finish(Minor changes required like Loader and Better error handling)

--- 12nexttruefeedback/src/app/(auth)/sign-in/page.tsx
+++ 12nexttruefeedback/src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,114 @@
-'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+'use client';
+import { useState } from 'react';
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/components/ui/use-toast';
+import { Button } from '@/components/ui/button';
+import { Form, FormField, FormLabel, FormItem, FormDescription, FormControl, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Loader2 } from 'lucide-react';
+import { signInSchema } from '@/schemas/signInSchema';
+import { signIn } from 'next-auth/react';
+
+export default function Page() {
+    
+    const router = useRouter();
+    const { toast } = useToast();
+
+  // zod implementation
+    const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+        identifier: '',
+        password: '',
+    },
+    });
+
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) =>
+  {
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+
+    if (result?.error) {
+      if (result.error === 'CredentialsSignin') {
+        toast({
+          title: 'Login Failed',
+          description: 'Incorrect username or password' ,
+          variant: 'destructive'
+        })
+      } else {
+        toast({
+          title: 'Error',
+          description: result.error ,
+          variant: 'destructive'
+        })
+      }
+
+    }
+
+    if (result?.url) {
+      router.replace('/dashboard')
+    }
+
+  };
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-teal-600 px-4 py-2 hover:bg-teal-800" onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white text-black rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Join Anonymous Message</h1>
+          <p className="mb-4">Sign In to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/Username" {...field} />
+                  </FormControl>
+                  <FormDescription>Enter your email/username</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormDescription>Enter your strong password</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant={'secondary'} type="submit" >Sign In</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a Member?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
