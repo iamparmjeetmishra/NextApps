2e8fd66f2564b4eaadae6252c36642874ecfa692
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-27 23:38:17 +0530

    13NextAuth2->Features->Helpers-Mailer File is updated with typo

--- 13nextauthv2/src/helpers/mailer.ts
+++ 13nextauthv2/src/helpers/mailer.ts
@@ -1,53 +1,60 @@
-import User from '@/models/userModel';
-import nodemailer from 'nodemailer'
-import bcryptjs from 'bcryptjs'
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
 
-export const sendEmail = async ({ email, emailType, userId }: any) => {
-   try {
+export const sendEmail = async ({
+	email,
+	emailType,
+	userId,
+}: any) => {
+	try {
+		const hashedToken = await bcryptjs.hash(userId.toString(), 10);
 
-      const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+		if (emailType === "VERIFY") {
+			await User.findByIdAndUpdate(userId, {
+				verifyToken: hashedToken,
+				verifyTokenExpiry: Date.now() + 3600000,
+			});
+		} else if (emailType === "RESET") {
+			await User.findByIdAndUpdate(userId, {
+				forgotPasswordToken: hashedToken,
+				forgotPasswordTokenExpiry: Date.now() + 3600000,
+			});
+		}
 
-      if (emailType === 'VERIFY') {
-         await User.findByIdAndUpdate(userId, 
-            {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000}
-         )
-      } else if (emailType === "RESET") {
-         await User.findByIdAndUpdate(userId, 
-            {
-               forgotPasswordToken: hashedToken,
-               forgotPasswordTokenExpiry: Date.now() + 3600000
-            }
-         )
-      }
+		const transport = nodemailer.createTransport({
+			host: "sandbox.smtp.mailtrap.io",
+			port: 2525,
+			auth: {
+				user: process.env.MAILER_USER,
+				pass: process.env.MAILER_PASSWORD,
+			},
+		});
 
-      const transport = nodemailer.createTransport({
-         host: "sandbox.smtp.mailtrap.io",
-         port: 2525,
-         auth: {
-            user: process.env.MAILER_USER,
-            pass: process.env.MAILER_PASSWORD
-         }
-      });
-
-      const mailOptions = {
-         from: 'info@parmjeetmishra.com',
-         to: email,
-         subject: emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password',
-         html: 
-            `
-               <p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">Here</a> to ${emailType === "VERIFY" ? 'verify your email' : 'Reset your password'}
+		const mailOptions = {
+			from: "info@parmjeetmishra.com",
+			to: email,
+			subject:
+				emailType === "VERIFY"
+					? "Verify your email"
+					: "Reset your password",
+			html: `
+               <p>Click <a href="${
+									process.env.DOMAIN
+								}/verifyemail?token=${hashedToken}">Here</a> to ${
+				emailType === "VERIFY"
+					? "verify your email"
+					: "Reset your password"
+			}
                or copy and paste the link below in your browser.<br>
                ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
                </p>,
-            `
-      };
+            `,
+		};
 
-
-      const mailResponse = await transport.sendMail(mailOptions)
-      return mailResponse;
-
-   } catch (error: any) {
-      throw new Error(error.message)
-   }
-}
-
+		const mailResponse = await transport.sendMail(mailOptions);
+		return mailResponse;
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
+};
