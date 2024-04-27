32f1913744609b1a10f7057807279939cf39d5a6
Parmjeet <iamparmjeetmishra@gmail.com>
2024-04-27 18:52:43 +0530

    13NextAuth2->Feature->Login Route setup with jwt Token

---
+++ 13nextauthv2/src/app/api/users/login/route.ts
@@ -0,0 +1,69 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+	try {
+		const reqBody = await request.json();
+		const { email, password } = reqBody;
+
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return NextResponse.json(
+				{ error: "User does not exist" },
+				{ status: 400 }
+			);
+		}
+		console.log("User Exists", user);
+
+		const validPassword = await bcryptjs.compare(
+			password,
+			user.password
+		);
+
+		if (!validPassword) {
+			return NextResponse.json(
+				{ error: "Check your password" },
+				{ status: 400 }
+			);
+		}
+
+		// jwt token
+
+		const tokenData = {
+			id: user._id,
+			username: user.username,
+			email: user.email,
+		};
+
+		const token = jwt.sign(
+			tokenData,
+			process.env.TOKEN_SECRET!,
+			{ expiresIn: "1d" }
+      );
+      
+      const response = NextResponse.json({
+         message: 'Logged in Success',
+         success: true
+      })
+
+      response.cookies.set(
+         "token",
+         token,
+         {httpOnly: true}
+      )
+
+	} catch (error: any) {
+		return NextResponse.json(
+			{
+				error: error.message,
+			},
+			{ status: 500 }
+		);
+	}
+}
