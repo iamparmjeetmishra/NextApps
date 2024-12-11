6ae3da4a8b763ccd44d8a2947f524c3d3007da52
Parmjeet <iamparmjeetmishra@gmail.com>
2024-12-11 19:53:03 +0530

    "Card added and tsconfig updated for baseurl and path"

--- 21expensetracker/frontend/src/App.tsx
+++ 21expensetracker/frontend/src/App.tsx
@@ -1,9 +1,32 @@
 import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default function App() {
   return (
-    <div className="text-3xl">
+    <div>
       <Button>Button</Button>
+      <Card className={"w-[380px]"} >
+      <CardHeader>
+        <CardTitle>Notifications</CardTitle>
+        <CardDescription>You have 3 unread messages.</CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+    I am content
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">
+          Mark all as read
+        </Button>
+      </CardFooter>
+    </Card>
     </div>
   )
 }
