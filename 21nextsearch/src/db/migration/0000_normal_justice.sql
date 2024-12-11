05b526026e30f750813eadacad553aba696c2e7f
Parmjeet <iamparmjeetmishra@gmail.com>
2024-12-11 18:44:01 +0530

    Project removed

--- 21nextsearch/src/db/migration/0000_normal_justice.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "searchProducts" (
-	"id" text PRIMARY KEY DEFAULT 'uuid_generate_v4()' NOT NULL,
-	"name" text NOT NULL,
-	"imageId" text NOT NULL,
-	"price" double precision NOT NULL,
-	"description" text,
-	"createdAt" timestamp DEFAULT now(),
-	"updatedAt" timestamp DEFAULT now()
-);
