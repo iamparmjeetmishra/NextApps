7d9a3d94afa9c6de9a2a05016bba28609abef42b
Parmjeet <iamparmjeetmishra@gmail.com>
2024-12-12 18:10:35 +0530

    fetching data extracted from home

--- 21expensetracker/frontend/src/routes/index.tsx
+++ 21expensetracker/frontend/src/routes/index.tsx
@@ -14,19 +14,11 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { api } from "@/lib/api";
-import { useQuery } from "@tanstack/react-query";
 
-async function getTotalSpent() {
-	const res = await api.expenses["total-spent"].$get();
-	if (!res.ok) {
-		throw new Error("Failed to fetch data");
-	}
-	const data = await res.json();
-	console.log(data.totalExpense);
-	// console.log('data', data)
-	return data.totalExpense;
-}
+import { useQuery } from "@tanstack/react-query";
+import getTotalSpent from '@/lib/actions';
+
+
 
 export default function Index() {
 	const { isPending, error, data } = useQuery({
