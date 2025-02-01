968b2e5eff5575131fcc4110e01c2e8d2acb69fe
Parmjeet <iamparmjeetmishra@gmail.com>
2025-02-01 19:20:26 +0530

    21->BE->drizzle updated for postgres and removed sqlite

--- 21expensetracker/backend/drizzle.config.ts
+++ 21expensetracker/backend/drizzle.config.ts
@@ -5,9 +5,8 @@ import env from "@/env-runtime";
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  dialect: "turso",
+  dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
   },
 });
