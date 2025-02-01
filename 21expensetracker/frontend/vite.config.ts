a0db266f95e7c18e5d6434cc50db70e93242b247
Parmjeet <iamparmjeetmishra@gmail.com>
2025-02-01 19:23:59 +0530

    21->FE->package upgraded

--- 21expensetracker/frontend/vite.config.ts
+++ 21expensetracker/frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../backend/server"),
+      "@server": path.resolve(__dirname, "../backend/src"),
     },
   },
   server: {
