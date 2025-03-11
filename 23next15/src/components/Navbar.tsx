c9c8efcb7060c3d06eccd3444735761b016d2cfd
Parmjeet <iamparmjeetmishra@gmail.com>
2025-03-11 17:19:30 +0530

    Shadcn added

---
+++ 23next15/src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+const NavLinks = [
+	{
+		title: "Home",
+		slug: "/",
+	},
+	{
+		title: "Dashboard",
+		slug: "/dashboard",
+	},
+];
+
+export default function Navbar() {
+	return (
+		<nav className="py-5 flex items-center justify-between">
+			<div className="flex items-center gap-6">
+				<Link href={"/"}>
+					<h1 className="text-2xl font-semibold">
+						Next<span className="text-blue-500">Blog</span>
+					</h1>
+				</Link>
+				<div className="hidden sm:flex items-center gap-6">
+					{NavLinks.map((Nav) => (
+						<Link
+							key={Nav.slug}
+							href={Nav.slug}
+							className="text-sm font-medium hover:text-blue-500 transition-colors"
+						>
+							{Nav.title}
+						</Link>
+					))}
+				</div>
+			</div>
+			<div></div>
+		</nav>
+	);
+}
